40530b4c769f80b4adb3e86953b108eb9f9eb578
danrungg <daniel.rungger@outlook.com>
2022-06-02 18:35:01 +0200

    add mousedown and hold to select div

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-// create 256 divs
+// create 256 divs and format with css
 const container = document.querySelector(".grid-container");
 
 for (let i = 0; i < 256; i++) {
     container.innerHTML += `<div class="box box-${i}"></div>`;
 }
+
+// click and hold to change color
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+const boxes = document.querySelectorAll(".box");
+
+let isDrawing = false;
+
+boxes.forEach((box) => {
+    box.addEventListener("mousedown", (e) => {
+        isDrawing = true;
+        console.log(isDrawing);
+    });
+
+    box.addEventListener("mousemove", (e) => {
+        if (isDrawing === true) {
+            console.log(box);
+        }
+    });
+
+    window.addEventListener("mouseup", (e) => {
+        if (isDrawing === true) {
+            isDrawing = false;
+        }
+    });
+});
