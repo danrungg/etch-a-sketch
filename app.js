d1e6c142c8c13e210a32d25f74868734fdcb351c
danrungg <daniel.rungger@outlook.com>
2022-06-03 21:23:20 +0200

    add slider to change grid squares, but why is it so slow?

--- app.js
+++ app.js
@@ -1,31 +1,68 @@
-// create 256 divs and format with css
 const container = document.querySelector(".grid-container");
+const slider = document.getElementById("sizeSlider");
+const output = document.getElementById("output");
 
-for (let i = 0; i < 256; i++) {
-    container.innerHTML += `<div class="box box-${i}"></div>`;
+function changeSlider() {
+    slider.addEventListener(
+        "input",
+        function () {
+            container.innerHTML = "";
+            console.log(slider.value);
+            changeSize(slider.value);
+            drawing();
+        },
+        false
+    );
+}
+changeSlider();
+
+// create 256 divs and format with css
+function generateGrid(numberOfSquares = 16) {
+    for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+        container.innerHTML += `<div class="box box-${i}"></div>`;
+    }
+}
+
+// change grid to number
+function changeSize(numberOfSquares) {
+    container.style.gridTemplateColumns = `repeat(${numberOfSquares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${numberOfSquares}, 1fr)`;
+    generateGrid(numberOfSquares);
+}
+
+// input number
+// changeSize(16);
+
+////////////////////////////////////////////////////////////////
+// draw color, only black
+function drawColor(box) {
+    box.style.backgroundColor = "black"; // #333333
 }
 
 // click and hold to change color
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
-const boxes = document.querySelectorAll(".box");
 
-let isDrawing = false;
+function drawing() {
+    const boxes = document.querySelectorAll(".box");
 
-boxes.forEach((box) => {
-    box.addEventListener("mousedown", (e) => {
-        isDrawing = true;
-        console.log(isDrawing);
+    let isDrawing = false;
+
+    boxes.forEach((box) => {
+        box.addEventListener("mousedown", (e) => {
+            isDrawing = true;
+            drawColor(box);
+        });
+
+        box.addEventListener("mousemove", (e) => {
+            if (isDrawing === true) {
+                drawColor(box);
+            }
+        });
+
+        window.addEventListener("mouseup", (e) => {
+            if (isDrawing === true) {
+                isDrawing = false;
+            }
+        });
     });
-
-    box.addEventListener("mousemove", (e) => {
-        if (isDrawing === true) {
-            console.log(box);
-        }
-    });
-
-    window.addEventListener("mouseup", (e) => {
-        if (isDrawing === true) {
-            isDrawing = false;
-        }
-    });
-});
+}
